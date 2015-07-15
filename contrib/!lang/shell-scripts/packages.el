3796bddeefcfb7ba11a5649b7e78b30574016e9e
James Conroy-Finn <james@logi.cl>
2015-07-15 22:35:45 +0100

    Teach shell-scripts about common ZSH files

--- contrib/!lang/shell-scripts/packages.el
+++ contrib/!lang/shell-scripts/packages.el
@@ -15,6 +15,23 @@
     fish-mode
     ))
 
+;; Use sh-mode when opening `.zsh' files, and when opening Prezto runcoms.
+(dolist (pattern '("\\.zsh\\'"
+                   "zlogin\\'"
+                   "zlogout\\'"
+                   "zpreztorc\\'"
+                   "zprofile\\'"
+                   "zshenv\\'"
+                   "zshrc\\'"))
+  (add-to-list 'auto-mode-alist (cons pattern 'sh-mode)))
+
+;; When we open a `.zsh' file set the shell to ZSH.
+(add-hook
+ 'sh-mode-hook
+ (lambda ()
+   (if (string-match "\\.zsh$" buffer-file-name)
+       (sh-set-shell "zsh"))))
+
 (defun shell-scripts/init-fish-mode ()
   (use-package fish-mode
     :defer t))
